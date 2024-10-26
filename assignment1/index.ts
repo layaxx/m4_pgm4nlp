e3046100a2c9820895d0e78d312eda4ddf904e33
Yannick Lang <layaxx@gmx.de>
2024-10-26 09:02:06 +0200

    add solution to task 2c)

--- assignment1/index.ts
+++ assignment1/index.ts
@@ -1,3 +1,5 @@
+const LARGE_VALUE = 1_000_000
+
 function probabilityMass(k: number): number {
   if (k < 1) return 0
   // prob of k => hitting k-1 times anything but 1, then hitting 1
@@ -13,10 +15,31 @@ function probabilityMass(k: number): number {
 function expectedValue(): number {
   // weighted sum of values (weighted by probability)
   let sum = 0
-  for (let i = 0; i < 1_000_000; i++) {
+  for (let i = 0; i < LARGE_VALUE; i++) {
     sum += i * probabilityMass(i)
   }
   return sum
+}
+
+function entropy(): number {
+  // average information content: - sum(prob * log prob)
+  const sum = Array.from(
+    { length: 3000 }, // gives NaN for larger values
+    (_, i) => probabilityMass(i + 1) // ged rid of 0
+  ).reduce((prev, curr) => prev + curr * Math.log(curr), 0)
+  return -sum
+}
+
+function entropy2() {
+  let sum = 0
+
+  for (let i = 1; i < 3000; i++) {
+    // gives NaN for larger values
+    const prob = probabilityMass(i)
+    sum += prob * Math.log(prob)
+  }
+
+  return -1 * sum
 }
 
 ;(function main() {
@@ -25,4 +48,6 @@ function expectedValue(): number {
   }
 
   console.log("expected Value", expectedValue())
+
+  console.log("entropy", entropy(), entropy2())
 })()
