b839577fcaf86461e93c230d6f4053b7c137b33c
Yannick Lang <layaxx@gmx.de>
2025-01-23 11:18:44 +0100

    wip(assignment5): got message selection working

--- assignment5/inference.py
+++ assignment5/inference.py
@@ -1,3 +1,4 @@
+import sys
 from bayes import load, train, make_obs
 from factor_graph import from_bayesnet
 
@@ -56,6 +57,73 @@ def gibbs(fg, evidence, n_steps, temp=1):
         pred[var_to_be_updated] = random.choices(values, weights=agreements, k=1)[0]
 
     return pred
+
+
+def calculate_v2f_message(variable, f2v_messages, v2f_messages):
+    val = random.choice(list(variable._range))
+    message = {val: 1}
+    for valprime in variable._range:
+        if valprime != val:
+            message[valprime] = 0
+
+    return message
+
+
+def calculate_f2v_message(factor, v2f_messages, f2v_messages):
+    message = {"val": 1}
+
+    return message
+
+
+def find_calculable_message(
+    fg, unsent_f2v_messages, unsent_v2f_messages, f2v_messages, v2f_messages
+):
+    """
+    Find the next calculable message in a factor graph.
+
+    This function iterates over the unsent f2v and v2f messages to find the
+    next message that can be calculated. A message is considered
+    calculable if all the required incoming messages are present.
+
+    Args:
+        fg: The factor graph containing factors and variables.
+        unsent_f2v_messages (list): List of unsent factor-to-variable messages.
+        unsent_v2f_messages (list): List of unsent variable-to-factor messages.
+        f2v_messages (set): Set of sent factor-to-variable messages.
+        v2f_messages (set): Set of sent variable-to-factor messages.
+
+    Returns:
+        tuple: A tuple containing the calculable message and a boolean flag
+               indicating the type of message. The boolean flag is True if
+               the message is a factor-to-variable message, and False if it
+               is a variable-to-factor message.
+    """
+
+    for message in unsent_f2v_messages:
+        factor_name, variable_name = message
+        factor = fg.factors[factor_name]
+        variable = fg.variables[variable_name]
+
+        # check every node in the factor but the target node has sent a message
+        if all(
+            (v.name, factor_name) in v2f_messages
+            for v in factor.variables
+            if v != variable
+        ):
+            return message, True
+
+    for message in unsent_v2f_messages:
+        variable_name, factor_name = message
+        variable = fg.variables[variable_name]
+        factor = fg.factors[factor_name]
+
+        # check if every connected factor but the target factor has sent a message
+        if all(
+            (f.name, variable_name) in f2v_messages
+            for f in variable.factors
+            if f != factor
+        ):
+            return message, False
 
 
 def belief_prop(fg, evidence, temp=1):
@@ -132,7 +200,7 @@ def belief_prop(fg, evidence, temp=1):
 
     USEFUL PIECES OF SYNTAX:
         If a factor fact has variables v1, v2, and v3, and you want to compute
-        the value of that vactor for the assignment (v1=val1, v2=val2, v3=val3),
+        the value of that vector for the assignment (v1=val1, v2=val2, v3=val3),
         use:
             fact.f((val1, val2, val3))
 
@@ -145,7 +213,41 @@ def belief_prop(fg, evidence, temp=1):
     """
 
     # YOUR CODE HERE!
-    raise NotImplementedError
+
+    # use itertools.product
+
+    unsent_v2f_messages = set(v2f_message_names) - set(v2f_messages.keys())
+    unsent_f2v_messages = set(f2v_message_names) - set(f2v_messages.keys())
+
+    # raise NotImplementedError
+
+    # while there are unsent messages left
+    while len(unsent_f2v_messages) + len(unsent_v2f_messages) > 0:
+        response = find_calculable_message(
+            fg, unsent_f2v_messages, unsent_v2f_messages, f2v_messages, v2f_messages
+        )
+
+        if response is None:
+            print(f2v_messages.keys())
+            print(v2f_messages.keys())
+            raise Exception("No message to calculate, but still unsent messages left")
+
+        message_to_calculate, is_factor_message = response
+
+        print(message_to_calculate, is_factor_message)
+
+        first, second = message_to_calculate
+
+        if is_factor_message:
+            f2v_messages[first, second] = calculate_f2v_message(
+                fg.factors[first], v2f_messages, f2v_messages
+            )
+            unsent_f2v_messages.remove(message_to_calculate)
+        else:
+            v2f_messages[first, second] = calculate_v2f_message(
+                fg.variables[first], f2v_messages, v2f_messages
+            )
+            unsent_v2f_messages.remove(message_to_calculate)
 
     """
     Now all messages have been passed. Let's calculate our marginals
@@ -210,7 +312,9 @@ if __name__ == "__main__":
             bp_pred_obs.append(bp_pred)
 
             print("BP Accuracy so far:", eval(bp_pred_obs, gold_obs))
+
         except NotImplementedError:
+            sys.exit(0)
             pass
 
         gibbs_pred = gibbs(factor_graph, observation, 2000, temp=1)
